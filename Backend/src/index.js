c13e0a35014d6933d57e1140ffb6f522097ac760
computergeek-ashwani-coder <168176169+computergeek-ashwani-coder@users.noreply.github.com>
2026-07-29 23:46:53 +0530

    Inital Remote Interview Platform Project Setup

---
+++ Backend/src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+// Create Express Server
+const app = express();
+
+// Middleware to parse the cross origin request
+app.use(cors());
+
+// Middleware to parse the json body
+app.use(express.json());
+
+//Routes 
+app.get("/" , (req , res) =>{
+    res.send("Welcome to the TalentIQ Backend");
+});
+
+// Routes to check the backend and Frontend connectivity
+app.get('/health' , (req , res)=>{
+    res.json({
+        success : true,
+        message : 'Backend is healthy'
+    });
+});
+
+
+const PORT = process.env.PORT  || 5000;
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+function App(){
+  return(
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <div className="text-center">
+          <h1 className="text-6xl font-bold">
+            Talent IQ
+          </h1>
+
+          <p className="text-gray-400 mt-5 text-xl">
+              Remote Interview Platform
+          </p>
+
+          <button className="mt-8 bg-blue-600 hover:bg-blue-700 px-8 py-3 rounded-lg font-semibold transition">
+            Get Started
+          </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
